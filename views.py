cee8a5dea889b2419711352dea10054fc6b728eb
Samus_ <mail2samus@gmail.com>
2010-08-12 00:47:03 -0300

    reducing to minimal case

--- views.py
+++ views.py
@@ -1,28 +1,26 @@
-from django.views.generic.simple import direct_to_template
-from django.shortcuts import redirect, get_object_or_404
+from django.forms import ModelForm
+from django.template import RequestContext
+from django.shortcuts import redirect, render_to_response, get_object_or_404
 
-def generic_edit_factory(model_class, form_class, template, redirect_func=None):
+def generic_edit_factory(modelform_class, template):
     """ factory function to generate views that handle create/edit on models and associated modelforms """
+    assert(isinstance(modelform_class, ModelForm))
 
-    def generic_edit_view(request, id=None, extra_context=None, **kwargs):
+    def generic_edit_view(request, id=None):
+        model_class = modelform_class.Meta.model
         model = get_object_or_404(model_class, pk=id) if id is not None else None
+
         if request.method == 'POST':
-            form = form_class(request.POST, instance=model)
+            form = modelform_class(request.POST, instance=model)
             if form.is_valid():
                 form.save()
 
-                if redirect_func is not None:
-                    redirect_to = redirect_func(form.instance)
-                else:
-                    redirect_to = form.instance.get_absolute_url()
+                redirect_to = form.instance.get_absolute_url()
                 return redirect(redirect_to)
         else:
-            form = form_class(instance=model)
+            form = modelform_class(instance=model)
 
-        if extra_context is None:
-            extra_context = {}
-        extra_context['form'] = form
-        return direct_to_template(request, template, extra_context=extra_context, **kwargs)
+        return render_to_response(template, {'form': form}, RequestContext(request))
 
     return generic_edit_view
 
